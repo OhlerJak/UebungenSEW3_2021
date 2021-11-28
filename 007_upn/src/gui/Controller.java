fb8fcb142e7823aefcd994f4bba832f787471b94
ohlerJak <jakob.ohler@htl-wels.at>
2021-11-28 19:18:43 +0100

    View fertig?

--- 007_upn/src/gui/Controller.java
+++ 007_upn/src/gui/Controller.java
@@ -1,4 +1,26 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
+    public TextField input;
+    public ListView lvstack;
+    public GridPane gpmain;
+
+
+
+
+    public void btfunconact() {
+
+
+    }
+
+    public void btentonact() {
+    }
+
+    public void btnronact() {
+    }
 }
