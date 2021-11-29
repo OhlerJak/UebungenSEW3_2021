af7b7db73dde355483060010ffa7f220ddc4df51
Jakob Ohler <jakob.ohler@htl-wels.at>
2021-11-29 13:37:45 +0100

    Nr buttons working

--- 007_upn/src/gui/Controller.java
+++ 007_upn/src/gui/Controller.java
@@ -1,26 +1,93 @@
 package gui;
 
+import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import javax.imageio.IIOException;
+import java.io.IOException;
 
 public class Controller {
-    public TextField input;
+    public TextField tfinput;
     public ListView lvstack;
     public GridPane gpmain;
+    public Button bt1;
+    public Button bt2;
+    public Button bt3;
+    public Button bt4;
+    public Button bt5;
+    public Button bt6;
+    public Button bt7;
+    public Button bt8;
+    public Button bt9;
+    public Button bt0;
 
 
+    public Button btplus;
+    public Button btminus;
+    public Button btmal;
+    public Button btdiv;
+    public Button btenter;
+
+    public TextField tfmes;
 
 
-    public void btfunconact() {
+    private FXMLLoader loader;
+    private Parent root;
+
+    public static  void launch(Stage stage)  {
+
+        Controller controller = new Controller();
+        controller.loader = new FXMLLoader(Controller.class.getResource("View.fxml"));
+
+        try{
+            controller.root =(Parent) controller.loader.load();
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+
+        Scene scene= new Scene(controller.root);
+        stage.setTitle("Rechner");
+        stage.setScene(scene);
+        stage.show();
+
 
 
     }
 
-    public void btentonact() {
+    public Controller(){
     }
 
-    public void btnronact() {
+    @FXML
+    public void initialize(){
+        showmes("Fertig erstellt! ");
     }
+
+    public void btfunconact(ActionEvent actionEvent) {
+        String text =  ((Button) actionEvent.getSource()).getText();
+        System.out.println(text);
+
+
+    }
+
+
+    public void btnronact(ActionEvent actionEvent) {
+       String text =  ((Button) actionEvent.getSource()).getText();
+        tfinput.setText(tfinput.getText()+text);
+
+    }
+
+    private void showmes(String mes){
+        tfmes.setText(mes);
+    }
+
 }
--- 007_upn/src/main/TheMain.java
+++ 007_upn/src/main/TheMain.java
@@ -1,12 +1,21 @@
 package main;
 
+import gui.Controller;
+import javafx.application.Application;
+import javafx.stage.Stage;
 import model.Rechner;
 import model.RechnerException;
 
-public class TheMain {
+public class TheMain extends Application {
     public static void main(String[] args) throws RechnerException {
 
-        Rechner rechner = new Rechner();
 
+        Rechner rechner = new Rechner();
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Controller.launch(primaryStage);
     }
 }
